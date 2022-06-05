10fe4727b3cc8b2d22d26840ab4f606c06f22545
Yunus <104511360+ZhongYah@users.noreply.github.com>
2022-06-05 14:34:39 +0800

    Add files via upload

---
+++ @Around/HpPrinter.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class HpPrinter implements Printer{
+
+    @Override
+    public void print(String message) {
+        System.out.println("HP印表機: " + message);
+    }
+}
